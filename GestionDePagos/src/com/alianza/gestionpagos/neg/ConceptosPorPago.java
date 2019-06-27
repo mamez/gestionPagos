a3354cdd23d7c07c08a9bbcf4ffae1e5b827fb82
mario.mendez <mario.mendez@grupocmc.co>
2019-06-26 20:53:38 -0500

    configuracion de bases de datos

--- GestionDePagos/src/com/alianza/gestionpagos/neg/ConceptosPorPago.java
+++ GestionDePagos/src/com/alianza/gestionpagos/neg/ConceptosPorPago.java
@@ -1,4 +1,10 @@
-package com.alianza.gestionpagos.neg;
+	package com.alianza.gestionpagos.neg;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,41 +14,63 @@ import org.tempuri.InformacionCuentas;
 import org.tempuri.InformacionFideicomisos;
 import org.tempuri.InformacionPagosSIFI;
 
+import com.alianza.gestionpagos.conexion.ConexionDirecta;
+import com.alianza.gestionpagos.conexion.ConexionType;
 import com.alianza.gestionpagos.exception.GestionPagosException;
+
+import oracle.jdbc.OracleTypes;
 
 public class ConceptosPorPago {
 
 	 private static final Logger log = LogManager.getLogger(GestionDePagosSoapImpl.class.getName());
 	 
 	 public org.tempuri.Conceptos[] getConceptosPorPago(java.lang.String codigoCaso) throws GestionPagosException {
-		 Conceptos c= new Conceptos();
-		 Conceptos c2= new Conceptos();
+		 log.debug("inicio del metodo getConceptosPorPago");
+		 Conceptos[] list ;
+		 ArrayList<Conceptos> listConceptos;
+		 Connection con=null;
+		 CallableStatement stmt=null;
+		 ResultSet rs=null;
 		 try {
-			 c.setCodigo_Pago("233");
-		       c.setTipo("Bases");
-		       c.setConcepto("ATC");
-		       c.setDescripcion("ANTICIPOS DE CONTRATO");
-		       c.setBase("23483127");
-		       c.setValor("23483127");
-		       
-		       
-		       c2.setCodigo_Pago("233");
-		       c2.setTipo("Bases");
-		       c2.setConcepto("ATC");
-		       c2.setDescripcion("ANTICIPOS DE CONTRATO");
-		       c2.setBase("23483127");
-		       c2.setValor("23483127");
-		
-		       
-	     }catch (Exception e) {
-			throw new GestionPagosException(e.getMessage());
-		}
+			 con= ConexionDirecta.getConexion(ConexionType.JDNI_PRUEBA);
+			 stmt = con.prepareCall("BEGIN prueba(?, ?); END;");
+			 stmt.setString(1,"BATCH");
+			 stmt.registerOutParameter(2, OracleTypes.CURSOR); 
+			 stmt.execute();
+			 rs = (ResultSet)stmt.getObject(2);
+			 listConceptos= new ArrayList<Conceptos>();
+			 while (rs.next()) {
+		    	 Conceptos c= new Conceptos();
+					String codigoComprobante=rs.getString("PAR_VALOR");
+					String descripcion=rs.getString("PAR_PARAMETRO");
+					   c.setCodigo_Pago("233");
+				       c.setTipo("Bases");
+				       c.setConcepto(codigoComprobante);
+				       c.setDescripcion(descripcion);
+				       c.setBase("23483127");
+				       c.setValor("23483127");
+				       listConceptos.add(c);
+		       }
+			 list= listConceptos.toArray(new Conceptos[listConceptos.size()]);
+		 }catch (SQLException e1) {
+				throw new GestionPagosException(e1.getMessage());
+			}catch(GestionPagosException e) {
+				throw new GestionPagosException(e.getMessage());
+			}catch (Exception e) {
+				throw new GestionPagosException(e.getMessage());
+			}finally {
+				try {
+					rs.close();
+					stmt.close();
+					con.close();
+				}catch (SQLException e) {
+					throw new GestionPagosException(e.getMessage());
+				}
+				
+			}
+			 log.debug("finalizando metodo getConceptosPorPago");
+		    	return list;
 		 
-		 log.debug("Finaliza correctamente el proceso");
-	    	
-	   
-	       Conceptos[] list = new Conceptos[]{c,c2}; 
-	    	return list;
 	    }
 	 
 	 
